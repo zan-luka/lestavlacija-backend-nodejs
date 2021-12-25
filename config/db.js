c1e19eb7a1b48d525c5618ce787a759863abbdde
zan-luka <zanluka.nusdorfer@gmail.com>
2021-12-25 10:42:01 +0100

    Deployment

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 module.exports = {
-  host: "localhost",
-  user: "yourusername",
-  password: "youruserpassword",
-  database: "yourdatabasename",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
